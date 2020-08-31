7845e9e2083d570822c3f05ddd1d47e409b4906d
Pallavi02-Test <60915081+Pallavi02-Test@users.noreply.github.com>
2020-08-31 15:49:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+
+var track,trackimg;
+var car,carimg;
+var border1,border2;
+
+function preload(){
+  trackimg = loadImage("images/roadtrack.png");
+  //carimg = loadImage("ambu.jpg");
+}
+
+function setup() {
+  createCanvas(displayWidth-20, displayHeight-30);
+  track = createSprite(displayWidth/2, 0,displayWidth, displayHeight+300);
+  track.addImage("track",trackimg);
+  track.y =  displayHeight/2 -400;
+  track.scale = 2;
+  
+  border1 = createSprite(displayWidth/2-215,0,5,displayHeight+900);
+  border1.y = displayHeight/2 -400;
+  border1.shapeColor = "red";
+
+  border2 = createSprite(displayWidth/2+220,0,5,displayHeight+900);
+  border2.y = displayHeight/2 -400;
+  border2.shapeColor = "red";
+
+}
+
+function draw() {
+  background(74,134,24); 
+  track.velocityY = 10;
+  border1.velocityY=10;
+  if(track.y > displayHeight){
+    track.y =  displayHeight/2 -400;
+  } 
+  if(border1.y > displayHeight){
+    border1.y =  displayHeight/2 -400;
+  } 
+  
+  drawSprites();
+}
